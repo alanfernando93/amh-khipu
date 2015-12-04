d8114a97e7a6af6f65276263e8c1056b2eb6a78e
Santiago G. Marín <santiago@finaldevstudio.com>
2015-12-04 17:55:54 -0300

    Initial commit

---
+++ test/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var expect = require('chai').expect;
+var crypto = require('crypto');
+var fikhipu = require('../');
+
+describe('Fi Khipu', function () {
+  before(function (done) {
+
+  });
+
+  describe('object', function () {
+    it('should be an object', function () {
+      expect(fikhipu).to.be.an.object();
+    });
+  });
+
+  after(function (done) {
+
+  });
+});
